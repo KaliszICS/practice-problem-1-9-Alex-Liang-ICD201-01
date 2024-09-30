8c0bb42bc3e425f94fe370dbf013b93436198ef5
Alex-Liang-ICD201-01 <alex.liang3@student.tdsb.on.ca>
2024-09-30 13:56:33 +0000

    commit

--- PP1_9.py
+++ PP1_9.py
@@ -3,23 +3,31 @@
 def q1(): 
   #Write Assignment code here
 
+  print("\"Hello World\"")
 
 def q2(): 
   #Write Assignment code here
 
+  word1 = input("Input a word: ")
+  print(word1.lower())
 
 def q3(): 
   #Write Assignment code here
 
+  word2 = input("Input a word that is at least 5 letters long: ")
+  print(word2.index(3:6))
 
 def q4(): 
   #Write Assignment code here
 
+  word3 = input("Input a word: ")
+  print(word3.index("o"))
 
 def q5(): 
   #Write Assignment code here
 
-
+  word4 = input("Input a word: ")
+  print(len(word4))
 
 #Do not alter the following code
 #Comment out the following code when running your tests
